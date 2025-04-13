2eb1682f51055b194ab6fcb157122fa47e99032c
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-04-13 20:27:37 +0300

    updated app.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ function SearchBar({ searchTerm, handleSearchChange }) {
   return (
     <input
       type="text"
-      placeholder="Search expenses..."
+      placeholder="Search your expenses..."
       value={searchTerm}
       onChange={handleSearchChange}
       className="search-bar"
